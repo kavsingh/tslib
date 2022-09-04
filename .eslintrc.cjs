46afc326aad787bcb2f5fa457f6345554d7e4b42
Kav Singh <flakisms@gmail.com>
2022-09-04 22:38:37 +0200

    add strict typescript lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -78,6 +78,7 @@ module.exports = {
 			extends: [
 				"plugin:@typescript-eslint/recommended",
 				"plugin:@typescript-eslint/recommended-requiring-type-checking",
+				"plugin:@typescript-eslint/strict",
 			],
 			rules: {
 				"camelcase": "off",
